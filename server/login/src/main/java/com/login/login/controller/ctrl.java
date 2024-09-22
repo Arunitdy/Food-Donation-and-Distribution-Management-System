588ea82ca4f97cb113a428147388b0eb1d066ca8
Aswaljith-P-R <120648896+Aswaljith-P-R@users.noreply.github.com>
2024-09-22 16:18:22 +0530

    errorreduced

--- server/login/src/main/java/com/login/login/controller/ctrl.java
+++ server/login/src/main/java/com/login/login/controller/ctrl.java
@@ -2,6 +2,42 @@ package com.login.login.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import com.login.login.repository.loginrepo;
+import com.login.login.model.Login_details;
+
+@RestController  // Defines this as a REST controller
+@RequestMapping("/login")  // Base URL for the login-related endpoints
+public class ctrl {
+
+    @Autowired  // Injects the repository into the controller
+    private loginrepo loginRepository;
+
+    // POST request to add new login details
+    @PostMapping("/add")
+    public Login_details addLoginDetails(@RequestBody Login_details loginDetails) {
+        return loginRepository.save(loginDetails);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*package com.login.login.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import com.login.login.model.Login_details;
 @RestController
@@ -14,4 +50,4 @@ public class ctrl {
     public Login_details addLogin(@RequestBody Login_details login_details) {
         return donorRepository.save(login_details);
     }
-} 
+} */
