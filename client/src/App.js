eb8a88564439a64db0aafbd4c4d0e260ba3c8a36
Arun <arunmundakkal003@gmail.com>
2024-09-25 02:10:36 +0530

    Revert "ok"
    This reverts commit 0bb3767b73cca64ea561eedbc9613ab99d1f2e6c, reversing
    changes made to 37b7538876d1ae89f28627f4cb2ce6fab16057b2.

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,61 @@
-import React, { useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import { Login } from './Login/Login';
-import { Signup } from './Signup/Signup';
-import { Home } from './Home/Home';
-import { Navbar } from './Navbar/Navbar';
-import { Footer } from './Footer/Footer';
+import { Login } from "./Login/Login.js";
+
+// Custom hook to handle event listener
+function useSignButtonListener(ref, setLoginVisible) {
+  useEffect(() => {
+    const refCurrent = ref.current;
+    console.log("refCurrent:",refCurrent)
+    const handleClick = () => {
+      console.log("sing button pressed")
+      setLoginVisible(true);  // Show the Login component
+    };
+
+    if (refCurrent) {
+      refCurrent.addEventListener('click', handleClick);
+    }
+
+    return () => {
+      if (refCurrent) {
+        refCurrent.removeEventListener('click', handleClick);
+      }
+    };
+  }, [ref, setLoginVisible]);
+}
 
 function App() {
-  const [loginSiginin, setLoginSiginin] = useState(true);
-  const [signupVisible, setSignupVisible] = useState(false);
-  const [loginVisible, setLoginVisible] = useState(false);
+  const [loginVisible, setLoginVisible] = useState(false); // Manage visibility of Login
+  const signButtonRef = useRef(null);
+
+  // Use custom hook for cleaner code
+  useSignButtonListener(signButtonRef, setLoginVisible);
 
   return (
-    <div className="App">
-      <Navbar 
-        loginSiginin={loginSiginin} 
-        setSignupVisible={setSignupVisible} 
-        setLoginVisible={setLoginVisible} 
-      />
-      
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* Removed login and signup from routes */}
-      </Routes>
-
-      {/* Only show the modals when their respective states are true */}
-      {signupVisible && (
-        <Signup 
-          setSignupVisible={setSignupVisible} 
-          setLoginSiginin={setLoginSiginin} 
-        />
-      )}
-      {loginVisible && (
-        <Login 
-          setLoginSiginin={setLoginSiginin} 
-          setLoginVisible={setLoginVisible} 
-        />
-      )}
-      
-      <Footer />
-    </div>
+  <div className="App">
+    <div> 
+      {loginVisible && <Login setLoginVisible={setLoginVisible} />} {/* Render Login when visible */}
+          
+        <div className='NavBar'>
+          {/* NavButton group */}
+          <div className='NavButton'>
+            <button className='Home individual-NavButton'>Home</button>
+            <button className='Donor individual-NavButton'>Donor</button>
+            <button className='Center individual-NavButton'>Center</button>
+            <button className='Receiver individual-NavButton'>Receiver</button>
+            <button className='Employee individual-NavButton'>Employee</button>
+          </div>
+          
+          {/* New container for Create Account link and Login button */}
+          <div className="NavBar-Auth">
+            <a className='navBar-a-creat-new-account' href="#">
+              Create a new account
+            </a>
+            <button className='Login-button-navBar' ref={signButtonRef}>Login</button>
+          </div>
+        </div>
+      </div>
+  </div>
   );
 }
 
