5bc2aa9ebf78e6cdb4996bd6f6e29ead84bac7d8
Arun <arunmundakkal003@gmail.com>
2024-10-08 18:02:30 +0530

    home

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -4,8 +4,8 @@ import './Home.css';
 export const Home = () => {
   return (
     <div className="home">
+      <img src="https://nss-new-add-media.s3.ap-south-1.amazonaws.com/NSS_USA_site/2023/10/16104028/blogfooddonationdrice.jpg"></img>
       <h1 className='content'>Welcome to FoodCare</h1>
-      {/* Add more content here */}
     </div>
   );
 };
